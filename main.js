4d7f608d0609d9480accda385c5224bb9e1b4255
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-08-19 16:39:49 -0300

    Upgrade

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 'use strict';
 
-/*  <label class="todo_item">
-    <input type="checkbox">
-    <div>item 1 test</div>
-    <input type="button" value="X">
-</label>
- */
-
-const createItem = () => {
+const createItem = (text, status='') => {
     const item = document.createElement('label')
     item.classList.add('todo_item');
     item.innerHTML = `
-         <input type="checkbox">
-         <div>item 1 test</div>
+         <input type="checkbox" ${status}>
+         <div>${text}</div>
          <input type="button" value="X">
     `
     document.getElementById('todoList').appendChild(item);
