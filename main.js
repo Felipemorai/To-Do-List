60279c96bb37ae70ffa12ec5c4d3f77f74192b39
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-08-19 09:48:58 -0300

    Upgrade

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*  <label class="todo_item">
+    <input type="checkbox">
+    <div>item 1 test</div>
+    <input type="button" value="X">
+</label>
+ */
+
+const criateItem = () => {
+    const item = document.createElement('label')
+}
